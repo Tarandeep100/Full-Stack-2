07e924ed58f93d4f942e6e846631404a8cbf97a1
unknown <tarandeepsingh100@gmail.com>
2022-04-09 23:31:26 -0400

    added lab - 1

---
+++ Lab - 1/my-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Greeter from './Greeter';
+import LikeButton from './LikeButton';
+
+const Display = () =>{
+  return <>
+    <Greeter/>
+    {Array(10).fill(<LikeButton />)}
+    
+  </>
+}
+ReactDOM.render(
+  // <React.StrictMode>
+  <>
+    <Display />
+  </>,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
