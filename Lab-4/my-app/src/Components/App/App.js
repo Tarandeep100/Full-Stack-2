9113ecda2d61b9a43ee7655d10a86f32d022d9b4
unknown <tarandeepsingh100@gmail.com>
2022-04-22 16:54:29 -0400

    added lab-4

---
+++ Lab-4/my-app/src/Components/App/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import UserList from '../UserList/UserList';
+// import AddStudent from '../AddStudent/AddStudent';
+// import DeleteStudent from '../DeleteStudent/DeleteStudent';
+
+function App() {
+  return (
+    <>
+      <UserList/>      
+    </>
+  );
+}
+
+export default App;
